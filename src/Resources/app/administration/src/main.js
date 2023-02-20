9d52dc13adb8ce32c299b69f9b4fdaa6137df247
der-andyman <coolandreaskoetter@googlemail.com>
2023-02-20 18:53:58 +0100

    Update main.js

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -2,3 +2,4 @@ import './module/salty-performance-analysis/api/salty-performance-analysis.servi
 import './module/salty-performance-analysis';
 import './module/salty-performance-analysis/component/salty-performance-analysis-grid';
 import './module/salty-performance-analysis/component/salty-performance-media-grid';
+import './module/salty-performance-analysis/component/salty-performance-content-grid';
